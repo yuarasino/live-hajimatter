445d7c3dd28be96d02e24ba5823f2415befdf1c2
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2022-11-20 10:24:41 +0900

    :electric_plug: pug, sassを導入

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -6,33 +6,27 @@ defineProps<{ msg: string }>()
 const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
+<template lang="pug">
+h1 {{ msg }}
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+div.card
+  button(type="button" @click="count++") count is {{ count }}
+  p
+    | Edit&nbsp;
+    code components/HelloWorld.vue
+    | &nbsp;to test HMR
+  p
+    | Check out&nbsp;
+    a(href="https://vuejs.org/guide/quick-start.html#local" target="_blank") create-vue
+    | , the official Vue + Vite starter
+  p
+    | Install&nbsp;
+    a(href="https://github.com/johnsoncodehk/volar" target="_blank") Volar
+    | &nbsp;in your IDE for a better DX
+  p.read-the-docs Click on the Vite and Vue logos to learn more
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
+<style lang="sass" scoped>
+.read-the-docs
+  color: #888
 </style>
